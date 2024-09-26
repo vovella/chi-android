e5e0fd7bc12fa16bd6678ddd6ddf7cbb0b55af4a
Volodymyr <kostenko1983@gmail.com>
2024-09-26 18:16:44 +0300

    01/abstract-factory base

---
+++ src/abstract-factory.kt
@@ -0,0 +1,37 @@
+interface AbstractFactory{
+    fun createUserNotification(): AbstractUserNotification;
+    fun createAdminNotification():AbstractAdminNotification;
+
+}
+
+interface AbstractNotification{
+    fun getNotification(msg: String): String;
+}
+
+class AbstractUserNotification: AbstractNotification{
+    override fun getNotification(msg: String): String{
+        return "User notification: ${msg}";
+    }
+}
+
+class AbstractAdminNotification: AbstractNotification{
+    override fun getNotification(msg: String): String{
+        return "Admin notification: ${msg}. IP: 127.0.0.1. ";
+    }
+}
+
+
+class TextNotificationFactory: AbstractFactory{
+    override fun createUserNotification(): AbstractUserNotification{
+        return AbstractUserNotification();
+    }
+    override fun createAdminNotification(): AbstractAdminNotification{
+        return AbstractAdminNotification();
+    }
+}
+
+fun main(){
+    var factory = TextNotificationFactory();
+    val userNotifacation = factory.createUserNotification();
+    println(userNotifacation.getNotification("Security alert"));
+}
